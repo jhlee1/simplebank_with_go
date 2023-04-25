31838030d256a6dfcc8f5f7f8bf7e07575f19965
Joohan Lee <joohan@ascend.io>
2023-04-25 12:51:01 -0400

    checkin lecture 12

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,24 +2,23 @@ package db
 
 import (
 	"database/sql"
+	"github.com/jhlee1/simplebank/db/util"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:mysecretpassword@localhost:5432/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig(".") // "." because the config file is in the current folder
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
--- main.go
+++ main.go
@@ -4,18 +4,18 @@ import (
 	"database/sql"
 	"github.com/jhlee1/simplebank/api"
 	db "github.com/jhlee1/simplebank/db/sqlc"
+	"github.com/jhlee1/simplebank/db/util"
 	_ "github.com/lib/pq"
 	"log"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:mysecretpassword@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".") // "." because the config file is in the current folder
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server:", err)
